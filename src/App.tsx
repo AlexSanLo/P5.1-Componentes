088187c9c36e80418bc705847839a468343fd290
AlexSanLo <san.lope.alex@gmail.com>
2025-02-17 10:24:25 +0100

    Estructura

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-
+import Button from "./components/Button";
+import Card from "./components/Card";
+import Inputs from "./components/Input";
 
 function App() {
+    <>
+        <section>
+            <div>
+                <h3>Componentes Cards</h3>
+            </div>
+        </section>
 
+        <section>
+            <div>
+                <h3>Componentes Button</h3>
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <h3>Componentes Inputs</h3>
+            </div>
+        </section>
+    </>
 }
 
 export default App
