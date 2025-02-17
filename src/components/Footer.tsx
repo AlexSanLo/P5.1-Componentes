4c7a81ed0e7150670f7b0127f7be2131f76ef2bf
AlexSanLo <san.lope.alex@gmail.com>
2025-02-17 16:56:13 +0100

    Version con logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
       <>
-      <section className="bg-(--colores-primarios-marron-negrizo) text-(--typography-colores-blanco) py-4 text-(length:--typography-size-h4)">
-       <div className="flex justify-between mx-10 font-(--typography-size-h2)">
+      <section className="bg-(--colores-primarios-marron-negrizo) text-(--typography-colores-blanco) pb-5 pt-3 text-(length:--typography-size-h4)">
+       <div className="flex justify-between items-center mx-10 font-(--typography-size-h2)">
          <div>
            <img src="./assets/WebCreativos.png" alt="Logo" className="w-(--logo-width) h-(--logo-height)" />
          </div>
