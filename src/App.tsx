a715954b58117d0de34a85b786945dfeb2a3a9cc
AlexSanLo <san.lope.alex@gmail.com>
2025-02-17 11:51:30 +0100

    Inputs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import viteLogo from '/vite.svg'
 import Button from "./components/Button";
 //import Card from "./components/Card";
-//import Inputs from "./components/Input";
+import Inputs from "./components/Inputs";
 //import Header from "./components/Header";
 //import Footer from "./components/Footer";
 
@@ -11,20 +11,26 @@ function App() {
         <>
         <section className="my-10 mx-10 font-(--typography-peso-bold) text-(--typography-size-h2)">
             <div>
-                <h2>Componentes Cards</h2>
+                <h2 className="text-3xl">Componentes Cards</h2>
             </div>
         </section>
 
         <section className="my-10 mx-10 font-(--typography-peso-bold) text-(--typography-size-h2)">
             <div>
-            <h2>Componentes Button</h2>
+            <h2 className="text-3xl">Componentes Button</h2>
             <Button text="Login" actionClick={() => console.log("Logueado/Registrado")}/>
             </div>
         </section>
 
-        <section className="my-10 mx-10 font-(--typography-peso-bold) text-(--typography-size-h2)">
+        <section className="my-10 mx-10 text-(--typography-size-h2)">
             <div>
-                <h2>Componentes Inputs</h2>
+                <h2 className="text-3xl font-(--typography-peso-bold)">Componentes Inputs</h2>
+                <div className="gap-5 flex">
+                <Inputs placeholder="Nombre" id="1" actionSearch={(text) => console.log(text)}/>
+                <Inputs placeholder="Email" id="2" actionSearch={(text) => console.log(text)}/>
+                <Inputs placeholder="Telefono" id="3" actionSearch={(text) => console.log(text)}/>
+                <Inputs placeholder="Direccion" id="4" actionSearch={(text) => console.log(text)}/>
+                </div>
             </div>
         </section>
     </>
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,7 +1,15 @@
-function Button() {
+interface inputProps {
+    placeholder: string;
+    id: string;
+    actionSearch: (text: string) => void;}
+
+function input({placeholder, id, actionSearch}: inputProps) {
     return (
-      <div>Button</div>
+      <div>
+        <input className="border-2 border-black rounded-md px-2 font-(--typography-peso-regular)" 
+                id={id} type="text"  placeholder={placeholder} onChange={(e) => actionSearch(e.target.value)}/>
+      </div>
     )
   }
   
-  export default Button
+  export default input
